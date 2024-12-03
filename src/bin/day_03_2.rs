3c68bdda4103af8bf80b6cffee37fe1f41648c00
Henry Wetton <henry.wetton@phys.chem.ethz.ch>
2024-12-03 13:43:45 +0100

    Add optimizations to day 3, part 2

--- src/bin/day_03_2.rs
+++ src/bin/day_03_2.rs
@@ -6,38 +6,26 @@ const OUTPUT_MESSAGE: &str = "Calculated total";
 const INPUT_FILE: &str = "input/day_03.txt";
 
 fn remove_disabled_sections(memory: String) -> String {
-    let haystack = &(memory.clone());
-    
-    let do_regex = Regex::new(r"do\(\)").unwrap();
-    let dont_regex = Regex::new(r"don't\(\)").unwrap();
-
-    let mut switch_positions: Vec<Option<bool>> = (0..memory.len()).map(|_| None).collect();
-    switch_positions[0] = Some(true);
-    do_regex.find_iter(haystack).map(|m| m.end()).for_each(|i| switch_positions[i] = Some(true));
-    dont_regex.find_iter(haystack).map(|m| m.start()).for_each(|i| switch_positions[i] = Some(false));
-    // -> switch_positions is Some(true) where a "do" block starts, Some(false) where a "don't" block starts and None otherwise
-
     let mut include_current = true;
-    let mut memory_processed = String::new();
-    memory.chars().enumerate().for_each(|(i, char)| {
-        if let Some(new_include_current) = switch_positions[i] { include_current = new_include_current; }
-        if include_current { memory_processed.push(char); }
-    });
-
-    memory_processed
+    memory.chars().enumerate().filter_map(|(i, char)| {
+        if (i < memory.len() - 4) && (&memory[i..i+4] == "do()") { include_current = true; }
+        else if (i < memory.len() - 7) && (&memory[i..i+7] == "don't()") { include_current = false; }
+        if include_current { Some(char) } else { None }
+    }).collect()
 }
 
 fn calculate_result(lines: Lines<BufReader<File>>) -> Result<u32, ()> {
-    let mut full_text = String::new();
-    lines.for_each(|l| {
-        let line = l.expect("invalid string");
-        full_text.push_str(&line);
-    });
+    let full_text: String = lines.map(|l| l.unwrap()).collect();
     let enabled_memory = remove_disabled_sections(full_text);
     let instruction_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let total = instruction_regex.captures_iter(&enabled_memory).map(|c| c.extract()).map(|(_, [x, y])| {
-        &str::parse::<u32>(x).expect("invalid number") * &str::parse::<u32>(y).expect("invalid number")
-    }).sum();
+    let total = instruction_regex
+        .captures_iter(&enabled_memory)
+        .map(|c| {
+            let (_, [x, y]) = c.extract();
+            &str::parse::<u32>(x).expect("invalid number")
+                * &str::parse::<u32>(y).expect("invalid number")
+        })
+        .sum();
     Ok(total)
 }
 
